00d2b600a1776a76439e2c551baaab1d566d0ded
upomathasin <tahsinchowdhuryupoma@gmail.com>
2023-11-22 18:45:03 +0600

    first

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRq5rVeOVtJD5aEkhFVsvBj0CkXLbwZ9s",
+  authDomain: "heal-hub-e8013.firebaseapp.com",
+  projectId: "heal-hub-e8013",
+  storageBucket: "heal-hub-e8013.appspot.com",
+  messagingSenderId: "71100505831",
+  appId: "1:71100505831:web:83ea4f4f25a7dd15a9cc42",
+};
+
+export default firebaseConfig;
